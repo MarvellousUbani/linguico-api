b3d4cb40d612f7f103d77619614fd31dbf415c69
Marvellous Ubani <marvellousubani@gmail.com>
2020-09-09 09:22:07 -0700

    Revert "Hosting"

--- app/models/appointment.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Appointment < ApplicationRecord
-  belongs_to :user
-  belongs_to :teacher
-
-  # validations
-  validates_presence_of :desc, :appointment_date, :appointment_time
-end
